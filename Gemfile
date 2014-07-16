0c6ceb8a8fcc25a7cb60069feda259890c92d20e
Catherine O'Neill <cathzozo@gmail.com>
2014-07-16 09:43:57 -0500

    first pizza test for topping

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
 
+
 gem 'rspec', '~> 2.14.0'
 gem 'pry-byebug'
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -10,4 +10,11 @@ describe Topping do
   it "exists" do
     expect(Topping).to be_a(Class)
   end
+
+  describe '.initialize' do
+    it "creates a new topping with a name" do
+      topping = Topping.new("olives")
+      expect(topping.name).to eq('olives')
+    end
+  end
 end
